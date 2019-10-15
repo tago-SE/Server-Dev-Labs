30f7ef08a47cfa5318445783925611ac35330a32
Tiago <tiagor@kth.se>
2019-10-15 16:09:16 +0200

    template project

---
+++ spring-docker-template/src/main/java/main/Application.java
@@ -0,0 +1,20 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({"controllers", "services"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
