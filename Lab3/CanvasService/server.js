d00a985fa6d1ed66d23866ee4b1e8f8e900b8ca6
Tiago <tiagor@kth.se>
2019-10-20 21:27:12 +0200

    First Working Canvas Drawer from 'CodingTrain'

--- Lab3/CanvasService/server.js
+++ Lab3/CanvasService/server.js
@@ -1,36 +1,54 @@
-var express = require('express'),
-    app = express(),
-    http = require('http'),
-    socketIo = require('socket.io');
+// Based off of Shawn Van Every's Live Web
+// http://itp.nyu.edu/~sve204/liveweb_fall2013/week3.html
+
+// Using express: http://expressjs.com/
+var express = require('express');
+// Create the app
+var app = express();
+
+// Set up the server
+// process.env.PORT is related to deploying on heroku
+var server = app.listen(process.env.PORT || 3000, listen);
+
+// This call back just tells us that the server has started
+function listen() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://' + host + ':' + port);
+}
+
+app.use(express.static('public'));
 
 
-var server = http.createServer(app);
-var io = socketIo.listen(server);
-server.listen(4000);
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io')(server);
 
-// add directory with our static files
-app.use(express.static(__dirname + '/public'));
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection',
+  // We are given a websocket object in our function
+  function (socket) {
+  
+    console.log("We have a new client: " + socket.id);
+  
+    // When this user emits, client side: socket.emit('otherevent',some data);
+    socket.on('mouse',
+      function(data) {
+        // Data comes in as whatever was sent, including objects
+        console.log("Received: 'mouse' " + data.x + " " + data.y);
+      
+        // Send it to all other clients
+        socket.broadcast.emit('mouse', data);
+        
+        // This is a way to send to everyone including sender
+        // io.sockets.emit('message', "this goes to everyone");
 
-console.log("Server running on localhost:4000");
-
-// array of all lines drawn
-var line_history = [];
-
-
-// event-handler for new incoming connections
-io.on('connection', function(socket) 
-{
-    console.log('client was connected to port 4000');
-    // first send the history to the new client
-    for (var i in line_history) {
-        socket.emit('draw_line', { line: line_history[i] });
-    }
-
-    // add handler for message type "draw_line".
-    socket.on('draw_line', function(data) {
-        // add received line to history 
-        line_history.push(data.line);
-        // send line to all clients
-        io.emit('draw_line', { line: data.line });
+      }
+    );
+    
+    socket.on('disconnect', function() {
+      console.log("Client has disconnected");
     });
-});
+  }
+);
