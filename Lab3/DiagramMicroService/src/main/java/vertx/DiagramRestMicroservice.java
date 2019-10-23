5b48cf480729ed74f85703242bf92e2f1b58a7b2
Tiago <tiagor@kth.se>
2019-10-23 22:35:31 +0200

    None working diagram query

--- Lab3/DiagramMicroService/src/main/java/vertx/DiagramRestMicroservice.java
+++ Lab3/DiagramMicroService/src/main/java/vertx/DiagramRestMicroservice.java
@@ -6,6 +6,7 @@ import io.vertx.core.json.Json;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.CorsHandler;
 import models.UserDiagram;
 
 import java.util.List;
@@ -20,12 +21,25 @@ public class DiagramRestMicroservice extends AbstractVerticle {
 
     @Override
     public void start() {
+
         Router router = Router.router(vertx);
+
+        router.route().handler(CorsHandler.create(".*.")
+                .allowedMethod(io.vertx.core.http.HttpMethod.GET)
+                .allowedMethod(io.vertx.core.http.HttpMethod.POST)
+                .allowedMethod(io.vertx.core.http.HttpMethod.OPTIONS)
+                .allowedHeader("Access-Control-Request-Method")
+                .allowedHeader("Access-Control-Allow-Credentials")
+                .allowedHeader("Access-Control-Allow-Origin")
+                .allowedHeader("Access-Control-Allow-Headers")
+                .allowedHeader("Content-Type"));
+
         router.route("/api/diagrams*").handler(BodyHandler.create());
         router.post("/api/diagrams").handler(this::insert);
         router.delete("/api/diagrams/delete/:id").handler(this::delete);
         router.get("/api/diagrams/get/:id").handler(this::get);
         router.get("/api/diagrams/get/user/:username").handler(this::getByUser);
+
         vertx.createHttpServer()
                 .requestHandler(router::accept)
                 .listen(7070);
@@ -63,20 +77,26 @@ public class DiagramRestMicroservice extends AbstractVerticle {
         }
     }
 
-    private void get(RoutingContext rc) {
-        final String id = rc.request().getParam("id");
-        if (id == null) {
-            rc.response().setStatusCode(400).end();
+private void get(RoutingContext rc) {
+    final String id = rc.request().getParam("id");
+    if (id == null) {
+        rc.response().setStatusCode(400).end();
+    } else {
+        UserDiagram diagram = userDiagramDao.get(id);
+        if (diagram == null) {
+            rc.response().setStatusCode(404).end();
         } else {
-            UserDiagram diagram = userDiagramDao.get(id);
-            if (diagram == null) {
-                rc.response().setStatusCode(404).end();
-            } else {
-                rc.response()
-                        .putHeader("content-type", "application/json; charset=utf-8")
-                        .end(Json.encodePrettily(diagram));
-            }
+            rc.response()
+                    .putHeader("content-type", "application/json; charset=utf-8")
+                    // Are these necessary?
+                    .putHeader("Access-Control-Allow-Origin", "*")
+                    .putHeader("Access-Control-Allow-Methods", "POST, GET")
+                    .putHeader("Custom-Header", "Own-Data")
+                    .putHeader("Access-Control-Expose-Headers", "Custom-Header")
+                    //
+                    .end(Json.encodePrettily(diagram));
         }
     }
+}
 
 }
